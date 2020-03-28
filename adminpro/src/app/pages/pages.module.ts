3969cbce8d7f9d07ffb0c3a48964960a2165a80c
David Robinson <david.robinson@logicstudio.net>
2020-03-28 16:16:05 -0500

    Sección 6: @inputs, @Outputs y @ViewChild

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { ChartsModule } from 'ng2-charts';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -8,12 +11,17 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages-routing.module';
 
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+        PagesComponent,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -23,7 +31,9 @@ import { PAGES_ROUTES } from './pages-routing.module';
     ],
     imports: [
         PAGES_ROUTES,
-        SharedModule
+        SharedModule,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule { }
