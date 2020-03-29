c065c433938b42e82f1d4e2a00adfb4f31768f66
David Robinson <david.robinson@logicstudio.net>
2020-03-29 14:42:14 -0500

    Sección 7: Servicios básicos, temas, rutas básicas y persistencia de los ajustes

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -13,6 +13,7 @@ import { PAGES_ROUTES } from './pages-routing.module';
 
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
     declarations: [
@@ -21,7 +22,8 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
         Graficas1Component,
         PagesComponent,
         IncrementadorComponent,
-        GraficoDonaComponent
+        GraficoDonaComponent,
+        AccountSettingsComponent
     ],
     exports: [
         DashboardComponent,
